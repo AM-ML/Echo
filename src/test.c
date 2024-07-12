7d4587c926326162c3faf44a2685f37b77af70b8
AM-ML <aliredamoumneh@gmail.com>
2024-07-12 15:14:34 +0300

    refactor test.c

--- src/test.c
+++ src/test.c
@@ -1,19 +1,7 @@
-#include <stdio.h>
 #include "defs.h"
 #include "bitboards.h"
 
 int main(void) {
-	U64 bb = 0ULL; addPiece(&bb, A2);
 
-	while(1)
-	{
-		Shift_R(&bb,A2, 4);
-		PrintBitBoard(bb);
-		Shift_U(&bb,E2, 4);
-		PrintBitBoard(bb);
-		Shift_DL(&bb,E6,4);
-		PrintBitBoard(bb);
-	}
-	
     return 0;
 }
