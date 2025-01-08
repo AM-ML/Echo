0af5cac57559618eac2e07397707de36f86ab8b2
am-ml <aliredamoumneh@gmail.com>
2025-01-08 09:35:41 +0200

    mod: ref files

--- ref/colSelector.py
+++ ref/colSelector.py
@@ -1,9 +1,9 @@
-def column(n):
+def col(n):
     # Generate a bitboard for the nth column (0-indexed)
     return sum(1 << (i * 8 + n) for i in range(8))
 
-def columns(*args):
+def cols(*args):
     total = 0
     for arg in args:
-        total += column(arg)
+        total += col(arg)
     return total
